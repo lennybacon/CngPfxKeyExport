a8db1cfd78eca066b1e4b7b38a1f46363a29eea9
Daniel Fisher <info@lennybacon.com>
2019-03-20 23:49:01 +0100

    Finished the plain text PKCS#8 export

--- src/Cng_MsTests/UnitTest1.cs
+++ src/Cng_MsTests/UnitTest1.cs
@@ -37,7 +37,9 @@ namespace Sample
               "4321",
               21);
 
-            
+            Assert.AreEqual(
+              Base64PrivateKey, 
+              Convert.ToBase64String(exportedKeyBytes));
 
 
           }
@@ -45,37 +47,34 @@ namespace Sample
       }
     }
 
-    public const string MicrosoftSoftwareKeyStorageProvider = "Microsoft Software Key Storage Provider";
+    public const string MicrosoftSoftwareKeyStorageProvider = 
+      "Microsoft Software Key Storage Provider";
 
     public const string Base64PrivateKey =
-      "MIIE+TAbBgoqhkiG9w0BDAEDMA0ECLh1R4milPMaAgEVBIIE2Gj2YaEZ/To1mXg" +
-      "Yg2ZM1Q6TOgEQHO0T1dxJaZj2T190c7o+O3r8oL1IGIeXWgomNCpB2jKQl4CO5w" +
-      "ybZfuYcSIT2Glq6nSm/rx9DjmSs/IIatM3sB5EN2BwJbO4FViegm4futdyj4ucS" +
-      "M1Xl9csW9lZVPPE0K91xy/nAx4fQHB9uLC4hwjtGDOPWe63zmWpvmxdBuRrt2sl" +
-      "1/7vKco+GFlDONi0wK0ZkJOueVYFby5ZcC7GCHvqJNEqGzTgJqesFVXbG+KYGK7" +
-      "nccUpAH7/ZQj6E+mjidQ2gCmj3hyo7fLbSaNpGhktN70ewyc973F6NuYVBWF/2W" +
-      "cvQfSSBJ6x7XuQXsV9ADzkjeJYXlqo7E9CV05sze7eqOhduwZNJeDPNNIo4VOSC" +
-      "JD4oeCiHCeQRiNLZE97nzKRcxZiq1kl11MSbOXMWGRKXz0gSW1xknDbLQiONtD5" +
-      "KHdplmokOTM2zEFD5o7QA5ZvRMUczLoX8EOUXuk5zTb9RAaIwhSLEBBw1F7l8BS" +
-      "rOJw3Kq3BBh/B5fYmqKAHVfS+QaS2o6Q3+WNshVqL50KENQpp6pPzWqIrDslo5h" +
-      "UN1DMK26nVRDJLgs3z1w0ppv1VWGKOrx8kTUDH8hUWSo6WJzzOp1QJsW/z/Klxy" +
-      "DRqtddOsWDKIEo4/+SkJHRaV/BKD+taGod4eyrHT/9+hcpOshuXsLbuvuEpzzDI" +
-      "L7slz7FOxAU8Jd8lGi/C0miMn4uXdQ4e6IatV+Y67Fs7o6H7eoJC1zH7F5IbgnU" +
-      "nLx/1ryLyheIGA07Qf6d/jwOaizbZ9Uol0rt6VIWvcDBtN/0ooXDZ6gmCs1kMuJ" +
-      "FK2zMuJLdX8qz4K77rNb623wg8cRJleA0OtFchLL2viBFOQDezrLiLgJA/21oTN" +
-      "+8gPLd8wteiXDn9VPqDOQNg7EoCM+sswey4QDZpv/+NRsiZt8T8wr0fI6IGsx0S" +
-      "3sco9uwgARIzjgbJHhO6iw2LOOrEaafZuTz9BQBDP72aqcYJI5zYx/OoBtx9h4a" +
-      "P6eTLe706SkXLrLgRgEkLGwHjCSQ71so9tbmxHOrNB8yuEg+w7bRL+Y1eByT4I8" +
-      "qIV2xsH2j/UsEBlPbyFEXTKomiFluxMqtNpcNDlDSNW2dEuNGmgG9vzf+sLG8sw" +
-      "0D6H3Jsc5FEcQ1zLXuND/J1ZyGaSJkOh3yzyH4B5fdhb+DgQCNgpNOfvGOlKOy8" +
-      "6qI5jEKcGrL8iFFRHDO3yTC7zyvzPP7aNhAuT/w2sbAYYpX+ZkShx4OgGDl8rja" +
-      "CxfeDiq2Ghn+qNhcX9aC0depe4p/SO1FwfIPJ8oEidG7itfB1n18HkxWy9OGWrT" +
-      "2omzAlXokqDyIObd8ofh64FtHNBCj1shv4YZ5l1dWecdEYbf6DcFXD400IAXzzL" +
-      "rEeIVCoQQEzGAdHnS82bfhBwMZY/r8iGjZflg5KfLqevENKBsWlrp5/u2cxR32Y" +
-      "y/tgFdSOlPeArG4oYyZvD2XtuzgP85u2Fms/vxg2+ufxDhNyPKSNXaK9B76t5i1" +
-      "DUzcMqS975BDtXCBBi3zukVRkBlEl7ksjpw8YZ5a4rTqjrEJw8qh3UX0lxa7UEO" +
-      "Wv+oLEwRlHgq+4VgJigMO0bkBpAW69gUddJmZSJR4j+0EmZfjI4cJpdQRrSFYmo" +
-      "v0=";
+      "MIIEzQIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCu7GSXCiCSkzRcF46k4" +
+      "AE64BKwxVmt0/353hqir+JDkhU00Mq+v7KgsKiFjO5jBNh5XcSN6OSDYfOzw9lVzUazNZ" +
+      "kOtfuZeShxl9L0mq7AOBPmnqfDDPt683qIdMtuR7V5bmZtHi69kuY8nPi2l7fxVZhwrVo" +
+      "AYi/4QrOWb0ZlUUSHbnCZ+aTbt/ad3YktkSS9vaRMvy+M6RIz4NRRvCJEsDXpFbb+qG8h" +
+      "0164Lshv86XlppUGRsc/vY4082vamASv/mt0NqhSqY/yEu5v3yCjzV9CXlQ9ZDCZex/zT" +
+      "6K5LScZ5bcxkEX/H9cx5JiasMT9ubq/Poxrh49Q6+clfrGxAgMBAAECggEAF+IGyeiEEf" +
+      "8vQvOkcjZzdIprgt8vI2CyaI8+q7+/8OHdK1zmkayywuXb3IAlfDeJJuj6kTWfHaEeGQl" +
+      "Af0qLwKQsxalrNMUqFHhaQTNtcKk3bmFzHlqDa4Ia1M69h80apf3A1l0snI2WCeiUvLly" +
+      "T+0T5y6D/1Foo0jYJE4FGogIOX3XxFnZ6BmgQ+ZVATW4ws1G0zW3N18Ou576USCcklqEq" +
+      "OoD3zdYrBi+qJbZQ4j1n/4tMIkXVDa1fZvzoh6XJsn11jJ47DiuFcgusYozFtJmlM0y0E" +
+      "wGrGMDZm0UtaIjDphtJFGEyjkR+sS+/E752fm0+9EuJxECE0bVnHoQkQKBgQDY/SqdNmU" +
+      "lb/ntq8h4HV6QRvMxTzYFaWMkiIQTrJhnRobNJuhiqqPpqurIBxgKY3g3z50Fs6XVjgij" +
+      "sl6V3uXzta9Ho4i2tA3J3lMFqoyIUV9HchkuysXUdqGpeLB2rM3RMGGfItq9K7xaMb9Oj" +
+      "zMlHSicE6lrez3L8TWtmgoaSwKBgQDOXy0+yM/xAMFX3WiyF47gNSizSomq2fz/yd5bP8" +
+      "b8e0UEaWkLR+jUjsknTfEJ99HMW0dStPsgO8a4QyQT8ynY+kb/MK9Xnz/QMRh4r4298XU" +
+      "l6ZFCW6ZJIKB6cXPwWRRbukR5MeFVOJ0o5HzmOVMbGcyLHWMtSn10+ZsxGJFmcwKBgQCv" +
+      "ZZ0/rUt/Qo9lBbmrFhptR1ynXB8PqsuAKXWRra8XtBOc2zl3H/YVGJpljA5rv1Ha01Ba3" +
+      "odLI19qavE39hAsuozvvInzHYITzkmPx/eG7Te2Oc2PXEsjXa15ntwNYcuQr6oJh5QG28" +
+      "aJpbK9HtW5x2MfHmmRPvKJ2vWH2X8XFwKBgQCekVr+huXf1Ci7DpDHZnqNo8rmGDu3X0+" +
+      "aas3DZznJ/h2FykjANjycNioxXR37/sLhNrGSxyS+G7ARHOW3vntFEOlEY3AW73Hk0hRv" +
+      "htRuQf01aEYIqssWHU+xRUNERe3ynSjHxp/RD3MSXJ3rd9h3Vldn/OibgETtgGzSSIJQK" +
+      "QKBgD6t7ixylbM05Z5XomSUA3+/txF4mHKyJLBoIL+9DESkJ1lWxNmytPcZ168cyEV9Q7" +
+      "Of01K63a5hObnFt2/9OaIqcTONmb3EbwNyvk8n8fB86Tz54xPqJNFd6FVG/LsacIjsDba" +
+      "w05ZExEkIaTRuo8twubjBdL5xTDBIlbOcoIOToA0wCwYDVR0PMQQDAgCQ";
 
     private static readonly RNGCryptoServiceProvider RandomNumberGenerator =
       new RNGCryptoServiceProvider();
@@ -95,8 +94,9 @@ namespace Sample
         pbeParams.Params.cbSalt = salt.Length;
         Marshal.Copy(salt, 0, (IntPtr)pbeParams.rgbSalt, salt.Length);
         pbeParams.Params.iIterations = kdfCount;
-
-        fixed (char* stringPtr = password)
+        var keyName = Guid.NewGuid().ToString("D");
+        fixed (char* passwordPtr = password)
+        fixed (char* keyNamePtr = keyName)
         fixed (byte* oidPtr = s_pkcs12TripleDesOidBytes)
         {
             NativeMethods.NCrypt.NCryptBuffer* buffers =
@@ -106,7 +106,7 @@ namespace Sample
             {
                 BufferType = NativeMethods.NCrypt.BufferType.PkcsSecret,
                 cbBuffer = checked(2 * (password.Length + 1)),
-                pvBuffer = (IntPtr)stringPtr,
+                pvBuffer = (IntPtr)passwordPtr,
             };
 
             if (buffers[0].pvBuffer == IntPtr.Zero)
@@ -178,15 +178,58 @@ namespace Sample
               MicrosoftSoftwareKeyStorageProvider,
               0);
 
+            NativeMethods.NCrypt.NCryptBuffer* buffers2 =
+              stackalloc NativeMethods.NCrypt.NCryptBuffer[4];
+
+            buffers2[0] = new NativeMethods.NCrypt.NCryptBuffer
+            {
+              BufferType = NativeMethods.NCrypt.BufferType.PkcsSecret,
+              cbBuffer = checked(2 * (password.Length + 1)),
+              pvBuffer = (IntPtr)passwordPtr,
+            };
+
+            if (buffers2[0].pvBuffer == IntPtr.Zero)
+            {
+              buffers2[0].cbBuffer = 0;
+            }
+
+            buffers2[1] = new NativeMethods.NCrypt.NCryptBuffer
+            {
+              BufferType = NativeMethods.NCrypt.BufferType.PkcsAlgOid,
+              cbBuffer = s_pkcs12TripleDesOidBytes.Length,
+              pvBuffer = (IntPtr)oidPtr,
+            };
+
+            buffers2[2] = new NativeMethods.NCrypt.NCryptBuffer
+            {
+              BufferType = NativeMethods.NCrypt.BufferType.PkcsAlgParam,
+              cbBuffer = sizeof(NativeMethods.NCrypt.PbeParams),
+              pvBuffer = (IntPtr)pbeParamsPtr,
+            };
+
+            buffers2[3] = new NativeMethods.NCrypt.NCryptBuffer
+            {
+              BufferType = NativeMethods.NCrypt.BufferType.PkcsKeyName,
+              cbBuffer = checked(2 * (keyName.Length + 1)),
+              pvBuffer = (IntPtr)keyNamePtr,
+            };
+
+            var desc2 = new NativeMethods.NCrypt.NCryptBufferDesc
+            {
+              cBuffers = 4,
+              pBuffers = (IntPtr)buffers2,
+              ulVersion = 0,
+            };
+
             result = NativeMethods.NCrypt.NCryptImportKey(
               safeNCryptProviderHandle,
               IntPtr.Zero,
               NCRYPT_PKCS8_PRIVATE_KEY_BLOB,
-              ref desc,
+              ref desc2,
               out var safeNCryptKeyHandle,
               exported,
               exported.Length,
-              0);
+              NCRYPT_DO_NOT_FINALIZE_FLAG);
 
             if (result != 0)
             {
@@ -210,42 +253,17 @@ namespace Sample
               safeNCryptKeyHandle,
               0);
 
-            result = NativeMethods.NCrypt.NCryptExportKey(
-              keyHandle,
-              IntPtr.Zero,
-              NCRYPT_PKCS8_PRIVATE_KEY_BLOB,
-              IntPtr.Zero,
-              null,
-              0,
-              out int bytesNeeded2,
-              0);
-
-            if (result != 0)
+            using (var key2 = CngKey.Open(keyName))
             {
-              throw new Win32Exception(result);
+              Trace.WriteLine(key2.ExportPolicy);
+
+              return key2.Export(CngKeyBlobFormat.Pkcs8PrivateBlob);
+
             }
-
-            byte[] exported2 = new byte[bytesNeeded2];
-
-            result = NativeMethods.NCrypt.NCryptExportKey(
-              safeNCryptKeyHandle,
-              IntPtr.Zero,
-              NCRYPT_PKCS8_PRIVATE_KEY_BLOB,
-              IntPtr.Zero,
-              exported2,
-              exported2.Length,
-              out bytesNeeded2,
-              0);
-
-            if (result != 0)
-            {
-              throw new Win32Exception(result);
-            }
-
-            return exported2;
         }
     }
 
+    private const int NCRYPT_DO_NOT_FINALIZE_FLAG = 0x00000400;
 
 
   }
